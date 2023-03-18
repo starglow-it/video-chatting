6171c42b346fbb9fdbd2056628becc838f629c6f
Tran Quoc Phi <>
2023-03-18 13:17:06 +0700

    fix error packages when build services

--- services/frontend-admin-service/server.js
+++ services/frontend-admin-service/server.js
@@ -23,7 +23,7 @@ const proxy = {
         changeOrigin: true,
     },
     '/socket.io': {
-        target: `http://socket-service:8080`,
+        target: `http://localhost:8080`,
         changeOrigin: true,
         ws: true,
     },
