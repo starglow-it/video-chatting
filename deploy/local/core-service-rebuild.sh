bb35198112742c7e7ef8c140bbc842a7b0820e48
Tran Quoc Phi <>
2023-03-28 11:44:51 +0700

    - delete bash files rebuild on this branch

--- deploy/local/core-service-rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-docker stop core-service
-docker rm -f core-service
-docker rmi -f local-core-service
-docker volume prune
-docker-compose up -d --build core-service
--- deploy/local/gateway-service-rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-docker stop gateway-service
-docker rm -f gateway-service
-docker rmi -f local-gateway-service
-docker volume prune
-docker-compose up --build -d gateway-service
