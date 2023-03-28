3afa180e4b27253068dddecfc1477e135b475132
Tran Quoc Phi <>
2023-03-28 11:41:29 +0700

    - delete bash files rebuild in the this branch

--- deploy/local/config-service-rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-docker stop config-service
-docker rm -f config-service
-docker rmi -f local-config-service
-docker volume prune
-docker-compose up --build -d config-service
--- deploy/local/media-server-service-rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!bin/bash
-docker stop media-server-service
-docker rm -f media-server-service
-docker rmi -f local-media-server-service
-docker volume prune
-docker-compose up --build -d media-server-service
