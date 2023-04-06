5cbfc9ed79731a66f6e63aed9cba3a59faa9ac51
Tran Quoc Phi <>
2023-04-06 17:43:04 +0700

    - update change logic config version

--- services/gateway-service/src/app.controller.ts
+++ services/gateway-service/src/app.controller.ts
@@ -1,18 +1,28 @@
-import { BadRequestException, Controller, Get, Logger } from "@nestjs/common";
+import { BadRequestException, Controller, Get, Logger, OnModuleInit } from "@nestjs/common";
 import { ConfigClientService } from "./services/config/config.service";
 
 @Controller()
-export class AppController {
+export class AppController implements OnModuleInit {
     constructor(private readonly configService: ConfigClientService) { }
+
     private readonly logger = new Logger();
+    private apiVersion: string;
+    private appVersion: string;
+    
+    async onModuleInit() {
+        this.apiVersion = await this.configService.get('apiVersion');
+        this.appVersion = await this.configService.get('appVersion');
+    }
+    
+
     @Get('versions')
     async getVersions() {
         try {
             return {
                 success: true,
                 result: {
-                    apiVersion: await this.configService.get('apiVersion'),
-                    appVersion: await this.configService.get('appVersion')
+                    apiVersion: this.apiVersion,
+                    appVersion: this.appVersion
                 }
             }
         }
