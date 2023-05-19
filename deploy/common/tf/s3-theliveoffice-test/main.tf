82fe8cb3bb2c51a184ab796e53ba875d8867fd79
hoangduc9375 <hoangduc9375@gmail.com>
2023-05-19 23:21:33 +0700

    update

--- deploy/common/tf/s3-theliveoffice-test/main.tf
+++ deploy/common/tf/s3-theliveoffice-test/main.tf
@@ -32,3 +32,26 @@ resource "aws_s3_bucket_public_access_block" "aws_s3_bucket" {
 #  ignore_public_acls      = false
 #  restrict_public_buckets = false
 }
+
+
+resource "aws_s3_bucket_policy" "aws_s3_bucket_policy" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+  policy = data.aws_iam_policy_document.aws_iam_policy_document.json
+}
+
+data "aws_iam_policy_document" "aws_iam_policy_document" {
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "${aws_s3_bucket.aws_s3_bucket.arn}/*",
+    ]
+  }
+}
