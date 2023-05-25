201b74f0d1408588212d7593ca3e85bfab1a06dd
Tran Quoc Phi <>
2023-05-25 17:52:06 +0700

    - update try catch for migration logic

--- services/core-service/src/database/database.service.ts
+++ services/core-service/src/database/database.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnApplicationBootstrap, OnModuleInit } from "@nestjs/common";
+import { Injectable, Logger, OnApplicationBootstrap, OnModuleInit } from "@nestjs/common";
 import { InjectConnection } from "@nestjs/mongoose";
 import { Connection, Model } from "mongoose";
 import { ConfigClientService } from "../services/config/config.service";
@@ -9,6 +9,7 @@ import { Collection, Environmens, EnvironmentMigration, MappingKey } from "share
 export class DatabaseService implements OnModuleInit, OnApplicationBootstrap {
     private environment: string;
     private models: Readonly<{ [index: string]: Model<any> }>;
+    private logger = new Logger(DatabaseService.name);
 
     constructor(
         @InjectConnection() private connection: Connection,
@@ -20,10 +21,16 @@ export class DatabaseService implements OnModuleInit, OnApplicationBootstrap {
     }
 
     async onApplicationBootstrap() {
-        if (this.connection.readyState !== 1) return;
-        this.models = this.connection.models;
+        try {
+            if (this.connection.readyState !== 1) return;
+            this.models = this.connection.models;
 
-        await this.syncDataMultipleEnvs();
+            await this.syncDataMultipleEnvs();
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     private isAcceptFilter(item: string): boolean {
@@ -31,131 +38,166 @@ export class DatabaseService implements OnModuleInit, OnApplicationBootstrap {
     }
 
     private async getRecords(entities: string[]): Promise<Collection[]> {
-        return (await Promise.all(entities.map(async modelName => {
+        try {
+            return (await Promise.all(entities.map(async modelName => {
 
-            const schemaObject = this.models[modelName]?.schema.obj || {};
+                const schemaObject = this.models[modelName]?.schema.obj || {};
 
-            const fieldList = Object.keys(schemaObject);
+                const fieldList = Object.keys(schemaObject);
 
-            const filterFields = (fieldList.flatMap(field => {
-                if (typeof schemaObject[field]['default'] !== 'undefined') {
-                    return {
-                        key: field,
-                        value: schemaObject[field]['default']
-                    }
-                }
-            })).filter(Boolean);
+                const filterFields = (fieldList.filter(field => typeof schemaObject[field]['default'] !== 'undefined'));
 
-            const records = await this.models[modelName]?.aggregate([{
-                $match: {
-                    $or: this.handleMappingValuesByKeys(fieldList, {
-                        $exists: false
-                    }).list
-                },
-            }, {
-                $set: {
-                    missKeys: {
-                        $setDifference: [
-                            filterFields.map(item => item.key),
-                            {
-                                $map:
-                                {
-                                    input: { $objectToArray: "$$ROOT" },
-                                    in: "$$this.k"
-                                }
-                            },
-                        ],
+                const records = await this.models[modelName]?.aggregate([{
+                    $match: {
+                        $or: this.handleMappingValuesByKeys(fieldList, {
+                            $exists: false
+                        }).list
                     },
-                },
-            }, {
-                $project: {
-                    _id: 1,
-                    missKeys: 1
+                }, {
+                    $set: {
+                        missKeys: {
+                            $setDifference: [
+                                filterFields,
+                                {
+                                    $map:
+                                    {
+                                        input: { $objectToArray: "$$ROOT" },
+                                        in: "$$this.k"
+                                    }
+                                },
+                            ],
+                        },
+                    },
+                }, {
+                    $project: {
+                        _id: 1,
+                        missKeys: 1
+                    }
+                }]).exec();
+                return {
+                    name: modelName,
+                    records
                 }
-            }]).exec();
-            return {
-                name: modelName,
-                records
-            }
-        }))) as Collection[];
+            }))) as Collection[];
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     async handleUpdateData(collections: Collection[]) {
-        await Promise.all(collections.map(async collection => {
-            collection.records.map(async record => {
+        try {
+            await Promise.all(collections.map(async collection => {
+                collection.records.map(async record => {
 
-                const updateData = this.handleMappingValuesByKeys(
-                    record.missKeys,
-                    record.missKeys.map(
-                        missKey => this.models[collection.name].schema.obj[missKey]['default']
-                    )
-                ).object;
+                    const updateData = this.handleMappingValuesByKeys(
+                        record.missKeys,
+                        record.missKeys.map(
+                            missKey => this.models[collection.name].schema.obj[missKey]['default']
+                        )
+                    ).object;
 
-                await this.models[collection.name].findByIdAndUpdate(
-                    record._id,
-                    {
-                        $set: {
-                            ...updateData
-                        }
-                    });
-            });
-        }));
+                    await this.models[collection.name].findByIdAndUpdate(
+                        record._id,
+                        {
+                            $set: {
+                                ...updateData
+                            }
+                        });
+                });
+            }));
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     private handleMappingValuesByKeys(keysArr: string[], value: any): MappingKey {
-        const mappingValue = (cur, index) => ({ [cur]: isArray(value) ? value[index] : value });
-        return {
-            list: keysArr.filter(item => this.isAcceptFilter(item)).reduce((acc, cur, index) => ([
-                ...acc,
-                mappingValue(cur, index)
-            ]), []),
-            object: keysArr.filter(item => this.isAcceptFilter(item)).reduce((acc, cur, index) => ({
-                ...acc,
-                ...mappingValue(cur, index)
-            }), {})
+        try {
+            const mappingValue = (cur, index) => ({ [cur]: isArray(value) ? value[index] : value });
+            return {
+                list: keysArr.filter(item => this.isAcceptFilter(item)).reduce((acc, cur, index) => ([
+                    ...acc,
+                    mappingValue(cur, index)
+                ]), []),
+                object: keysArr.filter(item => this.isAcceptFilter(item)).reduce((acc, cur, index) => ({
+                    ...acc,
+                    ...mappingValue(cur, index)
+                }), {})
+            }
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
         }
     }
 
     private async syncDataLocal() {
-        const entities = [
-            'User',
-            'CommonTemplate',
-            'UserTemplate',
-        ];
+        try {
+            const entities = [
+                'User',
+                'CommonTemplate',
+                'UserTemplate',
+            ];
 
-        const collections = await this.getRecords(entities);
-        await this.handleUpdateData(collections);
+            const collections = await this.getRecords(entities);
+            await this.handleUpdateData(collections);
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     private async syncDataDemo() {
-        const entities = [
-            'User',
-            'CommonTemplate',
-            'UserTemplate',
-        ];
+        try {
+            const entities = [
+                'User',
+                'CommonTemplate',
+                'UserTemplate',
+            ];
 
-        const collections = await this.getRecords(entities);
-        await this.handleUpdateData(collections);
+            const collections = await this.getRecords(entities);
+            await this.handleUpdateData(collections);
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     private async syncDataProd() {
-        const entities = [
-            'User',
-            'CommonTemplate',
-            'UserTemplate',
-        ];
+        try {
+            const entities = [
+                'User',
+                'CommonTemplate',
+                'UserTemplate',
+            ];
 
-        const collections = await this.getRecords(entities);
-        await this.handleUpdateData(collections);
+            const collections = await this.getRecords(entities);
+            await this.handleUpdateData(collections);
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 
     private async syncDataMultipleEnvs() {
-        const syncData: EnvironmentMigration = {
-            local: this.syncDataLocal,
-            demo: this.syncDataDemo,
-            production: this.syncDataProd
-        }
+        try {
+            const syncData: EnvironmentMigration = {
+                local: this.syncDataLocal,
+                demo: this.syncDataDemo,
+                production: this.syncDataProd
+            }
 
-        await syncData[this.environment ?? Environmens.Local].call(this);
+            await syncData[this.environment ?? Environmens.Local].call(this);
+        }
+        catch (err) {
+            this.logger.error(err.message);
+            return;
+        }
     }
 }
