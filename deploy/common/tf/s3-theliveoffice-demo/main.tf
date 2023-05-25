648f72b9749fe2aada212af48d4584e9755a7fc9
hoangduc9375 <hoangduc9375@gmail.com>
2023-05-25 11:35:35 +0700

    update

--- deploy/common/tf/s3-theliveoffice-demo/main.tf
+++ deploy/common/tf/s3-theliveoffice-demo/main.tf
@@ -32,3 +32,10 @@ resource "aws_s3_bucket_public_access_block" "aws_s3_bucket" {
 #  ignore_public_acls      = false
 #  restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
+  bucket = aws_s3_bucket.aws_s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
