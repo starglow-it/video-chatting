a0c54dcf7d898332f145febcd5d73be5f50574e2
hoangduc9375 <hoangduc9375@gmail.com>
2023-07-13 23:04:45 +0700

    update

--- deploy/common/tf/route53-chatruume-com/main.tf
+++ deploy/common/tf/route53-chatruume-com/main.tf
@@ -145,3 +145,11 @@ resource "aws_route53_record" "mandrill" {
   ttl     = 86400
   records = ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCrLHiExVd55zd/IQ/J/mRwSRMAocV/hMB3jXwaHH36d9NaVynQFYV8NaWi69c1veUtRzGt7yAioXqLj7Z4TeEUoOLgrKsn8YnckGs9i3B3tVFB+Ch/4mPhXWiNfNdynHWBcPcbJ8kjEQ2U8y78dHZj1YeRXXVvWob2OaKynO8/lQIDAQAB;"]
 }
+
+resource "aws_route53_record" "turn" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "turn.chatruume.com"
+  type    = "A"
+  ttl     = 86400
+  records = ["3.139.207.115"]
+}
