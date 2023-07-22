de0e39ffbb6d1da23b559d5bd31ffcb35d603e5c
Tran Quoc Phi <>
2023-07-22 19:46:50 +0700

    - update swagger

--- services/gateway-service/src/dtos/requests/delete-monitoring.request.ts
+++ services/gateway-service/src/dtos/requests/delete-monitoring.request.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Transform } from "class-transformer";
-import { IsArray, IsString } from "class-validator";
+import { IsDate, IsString } from "class-validator";
 
 export class DeleteMonitoringRequestDto {
     @ApiProperty({
         type: Date,
         description: 'This is the timestamp when the object is updated'
     })
-    @IsString()
+    @IsDate({
+        message: 'From must be Date'
+    })
     @Transform(({value}) => new Date(value))
     from: Date;
 }
