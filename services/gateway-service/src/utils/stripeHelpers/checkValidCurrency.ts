f24e7516fb58b41c79848e5f846a59f206b6786f
Phi2k <tranquocphipq@gmail.com>
2023-08-03 14:20:46 +0700

    - upercase charactor

--- services/gateway-service/src/utils/stripeHelpers/checkValidCurrency.ts
+++ services/gateway-service/src/utils/stripeHelpers/checkValidCurrency.ts
@@ -25,7 +25,7 @@ export const checkValidCurrency = async ({ amount, currency }: Args): Promise<vo
     try {
         if(!amount || !currency) return;
         const c = await convertCurrency(currency, amount);
-        const {new_amount} = await convertCurrency('USD', 0.5, currency);
+        const {new_amount} = await convertCurrency('USD', 0.5, currency.toUpperCase());
         if (c.new_amount <= 0.5) {
             throw new BadRequestException(`Invalid amount, please enter amount > ${new_amount}`);
         }
