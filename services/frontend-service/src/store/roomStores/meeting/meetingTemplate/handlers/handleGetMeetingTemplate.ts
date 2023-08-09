594dce7f4ab71dec77db69a175bb4dd264e83d29
Duy Phann <phanthanhduy.jin@gmail.com>
2023-08-09 10:21:22 +0700

    update

--- services/frontend-service/src/store/roomStores/meeting/meetingTemplate/handlers/handleGetMeetingTemplate.ts
+++ services/frontend-service/src/store/roomStores/meeting/meetingTemplate/handlers/handleGetMeetingTemplate.ts
@@ -5,13 +5,17 @@ import { initialTemplateState } from '../model';
 
 export const handleGetMeetingTemplate = async ({
     templateId,
+    subdomain,
 }: {
     templateId: IUserTemplate['id'];
     subdomain?: IUserTemplate['subdomain'];
 }): Promise<IUserTemplate> => {
-    const response = await sendRequest<IUserTemplate, ErrorState>(
-        getMeetingTemplateUrl({ templateId }),
-    );
+    const response = await sendRequest<IUserTemplate, ErrorState>({
+        ...getMeetingTemplateUrl({ templateId }),
+        data: {
+            subdomain,
+        },
+    });
 
     if (response.success) {
         return response.result;
