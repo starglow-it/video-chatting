213f955f21e969f8cd2fe71100019a691fe4d48a
Phi2k <tranquocphipq@gmail.com>
2023-10-06 11:05:56 +0700

    - fix error

--- services/meeting-socket-service/src/utils/ws/wsError.ts
+++ services/meeting-socket-service/src/utils/ws/wsError.ts
@@ -1,14 +1,18 @@
 type WsError = {
-  success: false,
-  message?: string
+  success: false;
+  message?: string;
 };
 
 export function wsError(clientId: string, message: string): WsError;
 export function wsError(clientId: string, error: unknown): WsError;
-export function wsError(clientId: string, error?: unknown, message?: string): WsError {
+export function wsError(
+  clientId: string,
+  error?: unknown,
+  message?: string,
+): WsError {
   console.error({
     ctx: clientId ? clientId : 'unknow',
-    error,
+    error: error ?? message,
   });
 
   return {
