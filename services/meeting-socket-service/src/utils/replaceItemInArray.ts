49a817b0d0b0532b82104a790667f5694f9f326b
Phi2k <tranquocphipq@gmail.com>
2023-10-07 20:04:35 +0700

    - fix user position and size mistakes. - fix disconnect and reconnect for lurker. - assign other room for lurker

---
+++ services/meeting-socket-service/src/utils/replaceItemInArray.ts
@@ -0,0 +1,19 @@
+import { isPrimitive } from './isPrimitiveType';
+
+export const replaceItemInArray = <T extends unknown>(
+  arr: T[],
+  searchItem: T,
+  replaceItem: T,
+) => {
+
+  let index = arr.indexOf(searchItem);
+  if (!isPrimitive(searchItem, true)) {
+    index = arr.findIndex(
+      (object) => JSON.stringify(object) === JSON.stringify(searchItem),
+    );
+  }
+
+  if (index === -1) return;
+  arr[index] = replaceItem;
+  return index;
+};
