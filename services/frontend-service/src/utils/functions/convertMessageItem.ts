e3a6d39ae7e51be46900ef4d35fce3dcef06b7df
Duy Phann <phanthanhduy.jin@gmail.com>
2023-10-12 19:01:06 +0700

    update

--- services/frontend-service/src/utils/functions/convertMessageItem.ts
+++ services/frontend-service/src/utils/functions/convertMessageItem.ts
@@ -2,12 +2,12 @@ import { MeetingChat } from 'src/store/types';
 
 export const convertMessageItem = (item: MeetingChat, messages: any[]) => {
     const tmp: any[] = [];
-    if (!messages.length)
-        tmp.push({
-            id: Date.now(),
-            body: new Date().toLocaleTimeString(),
-            type: 'time',
-        });
+    // if (!messages.length)
+    //     tmp.push({
+    //         id: Date.now(),
+    //         body: new Date().toLocaleTimeString(),
+    //         type: 'time',
+    //     });
     if (messages?.at(-1)?.sender?.id === item?.sender?.id) {
         tmp.push({ ...item, type: 'recently' });
     } else {
--- services/frontend-service/src/utils/functions/convertMessages.ts
+++ services/frontend-service/src/utils/functions/convertMessages.ts
@@ -2,7 +2,7 @@ import { MeetingChat } from 'src/store/types';
 
 export const convertMessages = (data: MeetingChat[]) => {
     const tmp: any[] = [];
-    tmp.push({ id: Date.now(), timestamp: Date.now(), type: 'time' });
+    // tmp.push({ id: Date.now(), timestamp: Date.now(), type: 'time' });
     data.forEach(item => {
         const lastItem = tmp.at(-1);
         if (lastItem?.id === item.id) {
