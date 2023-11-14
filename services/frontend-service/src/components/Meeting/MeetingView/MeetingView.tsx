8c67db26ecb28a934cbd2d4f1e70745973bcef20
Duy Phann <phanthanhduy.jin@gmail.com>
2023-11-14 14:08:57 +0700

    update

--- services/frontend-service/src/components/Meeting/MeetingView/MeetingView.tsx
+++ services/frontend-service/src/components/Meeting/MeetingView/MeetingView.tsx
@@ -45,6 +45,7 @@ import styles from './MeetingView.module.scss';
 
 // stores
 import {
+    $windowSizeStore,
     addNotificationEvent,
     appDialogsApi,
     checkIsPortraitLayoutEvent,
@@ -88,6 +89,7 @@ const Component = () => {
     const serverType = useStore($serverTypeStore);
     const isJoinMeetingPending = useStore(joinMeetingFx.pending);
     const { isMobile } = useBrowserDetect();
+    const { width, height } = useStore($windowSizeStore);
 
     const hostUser = useStoreMap({
         store: $meetingUsersStore,
@@ -183,6 +185,7 @@ const Component = () => {
     const previewImage = (meetingTemplate?.previewUrls || []).find(
         image => image.resolution === 1080,
     );
+    console.log('#Duy Phan console', previewImage);
 
     return (
         <CustomGrid className={styles.mainMeetingWrapper}>
@@ -193,27 +196,19 @@ const Component = () => {
             >
                 <CustomBox className={styles.imageWrapper}>
                     <ConditionalRender
-                        condition={!!previewImage?.url && !isMobile}
-                    >
-                        <CustomImage
-                            className={styles.image}
-                            src={previewImage?.url || ''}
-                            width="100%"
-                            height="100%"
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </ConditionalRender>
-                    <ConditionalRender
                         condition={
-                            isMobile && !!meetingTemplate?.mediaLink?.thumb
+                            !!previewImage?.url || !!meetingTemplate.mediaLink
                         }
                     >
                         <CustomImage
                             className={styles.image}
-                            src={meetingTemplate?.mediaLink?.thumb || ''}
-                            width="100%"
-                            height="100%"
+                            src={
+                                previewImage?.url ||
+                                meetingTemplate?.mediaLink?.thumb ||
+                                ''
+                            }
+                            width={isMobile ? `${width}px` : '100%'}
+                            height={isMobile ? `${height}px` : '100%'}
                             layout="fill"
                             objectFit="cover"
                         />
