e55738e8d31623bff4320b747427cb5f95e1e72a
Phi2k <tranquocphipq@gmail.com>
2023-12-06 11:14:43 +0700

    update comission fee

--- shared/shared-const/src/old/subscriptions.const.ts
+++ shared/shared-const/src/old/subscriptions.const.ts
@@ -43,7 +43,7 @@ const professionalSubscription: PlanData = {
     templatesLimit: 20,
     timeLimit: null,
     comissionFee: {
-      lurker: 0.05,
+      lurker: 0.2,
       participant: 0.05,
     },
   },
@@ -61,7 +61,7 @@ const businessSubscription: PlanData = {
     templatesLimit: 50,
     timeLimit: null,
     comissionFee: {
-      lurker: 0.5,
+      lurker: 0.1,
       participant: 0,
     },
   },
