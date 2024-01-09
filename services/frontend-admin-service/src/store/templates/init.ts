3fafaa1da130a56b344b886ab67b361ec8bd786f
starglow-it <starglowventures@gmail.com>
2024-01-08 19:04:11 -0800

    Fixed Console error in frontend-admin-service

--- services/frontend-admin-service/src/store/templates/init.ts
+++ services/frontend-admin-service/src/store/templates/init.ts
@@ -1,21 +1,13 @@
 import Router from 'next/router';
-import { ICommonTemplate } from 'shared-types';
 import { sample } from 'effector-next';
 
 import {
-    $activeTemplateIdStore,
-    $commonTemplates,
-    $commonTemplateStore,
-    $isUploadTemplateBackgroundInProgress,
     createFeaturedTemplateFx,
     createTemplateFx,
     deleteCommonTemplateFx,
     getCommonTemplateEvent,
     getCommonTemplateFx,
     getCommonTemplatesFx,
-    resetCommonTemplateStore,
-    setActiveTemplateIdEvent,
-    updateCommonTemplateDataEvent,
     updateCommonTemplateFx,
     uploadTemplateBackgroundFx,
 } from './model';
@@ -35,58 +27,6 @@ deleteCommonTemplateFx.use(handleDeleteCommonTemplate);
 updateCommonTemplateFx.use(handleUpdateCommonTemplate);
 uploadTemplateBackgroundFx.use(handleUploadCommonTemplateBackground);
 createFeaturedTemplateFx.use(handleCreateFeaturedTemplate);
-
-$commonTemplates
-    .on(getCommonTemplatesFx.doneData, (state, data) => data)
-    .on(updateCommonTemplateFx.doneData, (state, data) => ({
-        ...state,
-        state: {
-            ...state.state,
-            list: state.state.list.map(template =>
-                data.state && template.id === data.state?.id
-                    ? data.state
-                    : template,
-            ),
-        },
-    }))
-    .on(deleteCommonTemplateFx.done, (state, { params }) => ({
-        ...state,
-        state: {
-            count: state.state.count - 1,
-            list: state.state.list.filter(
-                template => template?.id !== params.templateId,
-            ),
-        },
-    }));
-
-$commonTemplateStore
-    .on(
-        [
-            createTemplateFx.doneData,
-            getCommonTemplateFx.doneData,
-            uploadTemplateBackgroundFx.doneData,
-            createFeaturedTemplateFx.doneData,
-        ],
-        (state, data) =>
-            data.state
-                ? data
-                : {
-                      ...state,
-                      error: data.error,
-                  },
-    )
-    .on(updateCommonTemplateDataEvent, (state, data) => ({
-        ...state,
-        state: {
-            ...state.state,
-            ...data,
-        } as ICommonTemplate,
-    }))
-    .reset([resetCommonTemplateStore]);
-
-$isUploadTemplateBackgroundInProgress.reset(resetCommonTemplateStore);
-
-$activeTemplateIdStore.on(setActiveTemplateIdEvent, (state, data) => data);
 
 createTemplateFx.doneData.watch(data => {
     if (data.state?.id) {
