e7e2938c7c4f92a72554863d96c8b1963ac23658
starglow-it <starglowventures@gmail.com>
2024-04-30 14:44:12 -0700

    handled exception of safari popup

--- services/frontend-service/src/store/roomStores/videoChat/sfu/handlers/handleStartSFUSharing.ts
+++ services/frontend-service/src/store/roomStores/videoChat/sfu/handlers/handleStartSFUSharing.ts
@@ -5,27 +5,31 @@ export const handleStartSFUSharing = async ({
     room,
     userId,
 }: StartSFUSharingPayload) => {
-    if (room) {
-        const { localParticipant } = room;
+    try {
+        if (room) {
+            const { localParticipant } = room;
 
-        const oldSharingTrack = localParticipant.getTrack(
-            Track.Source.ScreenShare,
-        );
+            const oldSharingTrack = localParticipant.getTrack(
+                Track.Source.ScreenShare,
+            );
 
-        if (oldSharingTrack?.videoTrack) {
-            localParticipant.unpublishTrack(
-                oldSharingTrack.videoTrack.mediaStreamTrack,
+            if (oldSharingTrack?.videoTrack) {
+                localParticipant.unpublishTrack(
+                    oldSharingTrack.videoTrack.mediaStreamTrack,
+                );
+            }
+
+            await localParticipant.setScreenShareEnabled(
+                true,
+                { audio: false },
+                {
+                    name: `sharing_${userId}`,
+                    source: Track.Source.ScreenShare,
+                    simulcast: true,
+                },
             );
         }
-
-        await localParticipant.setScreenShareEnabled(
-            true,
-            { audio: false },
-            {
-                name: `sharing_${userId}`,
-                source: Track.Source.ScreenShare,
-                simulcast: true,
-            },
-        );
+    } catch (error) {
+        console.log(error);
     }
 };
