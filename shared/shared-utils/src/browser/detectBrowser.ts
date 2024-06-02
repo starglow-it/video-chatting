1b610f8f4627040c531f167928c99cee25eb19df
starglow-it <starglowventures@gmail.com>
2024-06-01 19:04:35 -0700

    updated video logic for mobile

--- shared/shared-utils/src/browser/detectBrowser.ts
+++ shared/shared-utils/src/browser/detectBrowser.ts
@@ -7,5 +7,5 @@ export const isSafari = () =>
   parse(window.navigator.userAgent).browser.name === 'Safari';
 
 export const isMobile = () =>
-  // parse(window.navigator.userAgent).platform.type === 'mobile';
-  /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+  parse(window.navigator.userAgent).platform.type === 'mobile';
+  // /Mobi|Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
